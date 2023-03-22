19ec7562f44c00dedea577344c51f46fb21681ad
João Pedro <jaumgamer223@gmail.com>
2023-03-22 00:20:46 -0300

    Changed alt text for the recent episodes

--- src/components/episodeView/episodeView.tsx
+++ src/components/episodeView/episodeView.tsx
@@ -2,7 +2,7 @@ import "./episodeView.scss";
 export function EpisodeView({ title, image, episodeNumber }) {
   return (
     <div className="recentEpisode">
-      <img src={image} alt={`${title} image`} />
+      <img src={image} alt={`${title} episode ${episodeNumber}`} />
       <p>
         {title}: Episode {episodeNumber}
       </p>
