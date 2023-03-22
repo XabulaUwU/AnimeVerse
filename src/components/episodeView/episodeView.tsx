a1d27a99a5a1438619da36a964d4c3225f0384c4
João Pedro <jaumgamer223@gmail.com>
2023-03-22 00:18:40 -0300

    Added the recent episodes to the home page. No Functionality

--- src/components/episodeView/episodeView.tsx
+++ src/components/episodeView/episodeView.tsx
@@ -1,3 +1,11 @@
-export function EpisodeView() {
-  return;
+import "./episodeView.scss";
+export function EpisodeView({ title, image, episodeNumber }) {
+  return (
+    <div className="recentEpisode">
+      <img src={image} alt={`${title} image`} />
+      <p>
+        {title}: Episode {episodeNumber}
+      </p>
+    </div>
+  );
 }
--- src/components/main/mainView/mainView.tsx
+++ src/components/main/mainView/mainView.tsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
 import { data as recent } from "../../../functions/recentEpisodes/recentEpisodes";
+import { EpisodeView } from "../../episodeView/episodeView";
 import "./mainView.scss";
 type recentEpisodesList = {
   currentPage: 0;
   hasNextPage: true;
   results: [
     {
-      id: "string";
-      episodeId: "string";
+      id: string;
+      episodeId: string;
       episodeNumber: 0;
-      title: "string";
-      image: "string";
-      url: "string";
+      title: string;
+      image: string;
+      url: string;
     }
   ];
 };
@@ -28,9 +29,17 @@ export function MainView() {
   return (
     <main>
       <h1>Anime Schedule</h1>
-      <div id="schedule">{recentEpisodes?.currentPage}</div>
+      <div id="schedule">{1}</div>
       <h1>Recent Episodes</h1>
-      <div id="recent">Recent</div>
+      <div id="recent">
+        {recentEpisodes?.results.map((i) => (
+          <EpisodeView
+            title={i.title}
+            image={i.image}
+            episodeNumber={i.episodeNumber}
+          />
+        ))}
+      </div>
       <h1>Popular Anime</h1>
       <div id="popular">Popular</div>
     </main>
