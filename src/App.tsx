6e93b108fbe95ffe7a24dcfbf5dff1597e865112
João Pedro <jaumgamer223@gmail.com>
2023-03-24 12:16:41 -0300

    Created the anime info route

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import "./app.scss";
 import Home from "./layouts/home/home";
 import Watch from "./layouts/watch/watch";
+import AnimeInfo from "./layouts/anime/animeInfo";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="watch" element={<Watch />} />
+      <Route path="anime/:id" element={<AnimeInfo />} />
     </Routes>
   );
 }
---
+++ src/layouts/anime/animeInfo.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "../../components/footer";
+import { Header } from "../../components/header";
+
+function AnimeInfo() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default AnimeInfo;
