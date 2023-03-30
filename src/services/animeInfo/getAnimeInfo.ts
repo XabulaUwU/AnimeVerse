db410080ea51c9ec95d0b868b84dd8189a94683e
João Pedro <jaumgamer223@gmail.com>
2023-03-30 12:05:24 -0300

    Updated the animeInfo part to the new API

--- src/services/animeInfo/getAnimeInfo.ts
+++ src/services/animeInfo/getAnimeInfo.ts
@@ -1,10 +1,15 @@
 import { instance } from "../api/axios";
 
 // Using the example ID of "spy-x-family".
-const url = "https://api.consumet.org/anime/gogoanime/info/";
+const url = "anime";
 export const data = async (id: string) => {
   try {
-    const { data } = await instance.get(url + id);
+    const { data } = await instance.get(url, {
+      params: {
+        url: id,
+        site: "animefire",
+      },
+    });
     return data;
   } catch (err) {
     throw new Error(err.message);
--- src/views/InfoView/index.tsx
+++ src/views/InfoView/index.tsx
@@ -3,31 +3,32 @@ import { Link } from "react-router-dom";
 import { data as info } from "../../services/animeInfo/getAnimeInfo";
 import "./index.scss";
 type animeInfoObject = {
-  id: string;
   title: string;
-  url: string;
   image: string;
-  releaseDate: string | null;
-  description: string | null;
-  genres: [string];
-  subOrDub: string;
-  type: string | null;
-  status: string;
-  otherName: string | null;
-  totalEpisodes: number;
-  episodes: [
+  tags: [string];
+  description: string;
+  year: null | string;
+  rating: number;
+  seasons: [
     {
-      id: string;
-      number: number;
-      url: string;
+      title: string;
+      episodes: [
+        {
+          title: string;
+          url: string;
+          image: string;
+        }
+      ];
     }
   ];
+  related: [];
 };
 
 export function InfoView() {
   const [episodesInfo, setEpisodesInfo] = useState<animeInfoObject>();
   const getAnimeId = () => {
-    return window.location.href.slice(28);
+    console.log(window.location.href.slice(22));
+    return window.location.href.slice(22);
   };
   const animeInfo = async () => {
     const animeInfoList = await info(getAnimeId());
@@ -44,24 +45,25 @@ export function InfoView() {
       <div id="information">
         <div id="info">
           <h1>{episodesInfo?.title}</h1>
-          <h2>Synopses: {episodesInfo?.description}</h2>
-          <h2>Also Known as: {episodesInfo?.otherName}</h2>
-          <h2>Release Date: {episodesInfo?.releaseDate}</h2>
-          <h2>Status: {episodesInfo?.status}</h2>
-          <h2>Is subbed? {episodesInfo?.subOrDub === "sub" ? "yes" : "no"}</h2>
-          <h2>Total Episodes: {episodesInfo?.totalEpisodes}</h2>
-          <h2>Season: {episodesInfo?.type}</h2>
+          <h2>Sinopse: {episodesInfo?.description}</h2>
+          <h2>
+            Data de Lançamento:
+            {episodesInfo?.year ? ` ${episodesInfo?.year}` : " Não Informado"}
+          </h2>
+          <h2>
+            Número de episódios: {episodesInfo?.seasons[0].episodes.length}
+          </h2>
         </div>
         <img src={episodesInfo?.image} alt={episodesInfo?.title} />
       </div>
       <div id="episodes">
-        {episodesInfo?.episodes.map((i, index) => {
+        {episodesInfo?.seasons[0].episodes.map((i, index) => {
           return (
             <div id="episode">
               <p>
-                {episodesInfo?.title}: Episode {i.number}
+                {episodesInfo?.title}: Episode {i.title}
               </p>
-              <Link to={`/watch/${i.id}`}>
+              <Link to={`/watch${i.url}`}>
                 <p className="watch">Watch</p>
               </Link>
             </div>
