40f8ffb86093c563db23d0e49d93b956d62cd6b4
João Pedro <jaumgamer223@gmail.com>
2023-04-02 21:02:58 -0300

    Added the two player options

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: "https://anime-app-back.onrender.com/",
-  timeout: 10000,
+  timeout: 30000,
 });
--- src/views/WatchView/index.tsx
+++ src/views/WatchView/index.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import {MouseEvent,  useEffect, useState } from "react";
 import { data as episode } from "../../services/animeEpisode/episode";
 import ReactPlayer from "react-player";
 import { instance } from "../../services/api/axios";
 import { useLocation } from "react-router-dom";
+import './index.scss'
+
 type episodeObj = {
   url: string;
 };
@@ -11,9 +13,10 @@ export function WatchView() {
   const [episodeInfo, setEpisodeInfo] = useState<episodeObj>();
   const location = useLocation();
   const { episodes } = location.state;
+  
+    
   const getEpisodeId = () => {
     const urlEnd = window.location.href.indexOf("animes/");
-    console.log(window.location.href.slice(urlEnd));
     return window.location.href.slice(urlEnd);
   };
   const episodeData = async () => {
@@ -22,12 +25,30 @@ export function WatchView() {
     setEpisodeInfo(newEpisodeData);
   };
   useEffect(() => {
-    //episodeData();
-    console.log(episodes);
+    episodeData();
     // eslint-disable-next-line
   }, []);
+  const handleClick = (event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+    const episodePlayer = document.getElementById('player')
+    const otherButton = document.querySelector('.active')
+    otherButton.classList.toggle('active')
+    event.currentTarget.classList.toggle('active')
+    if(event.currentTarget.value === 'Player 2') {
+      console.log(event.currentTarget.value)
+      episodePlayer.innerHTML = `<div style="width: 640px; height: 360px;">
+      <video src=${episodeInfo.url} preload="auto" controls="" style="width: 100%; height: 100%;"></video>
+    </div>`
+    }
+    else{
+      episodePlayer.innerHTML = `<div style="width: 640px; height: 360px;">
+      <video src=${instance.defaults.baseURL}watch?url=${getEpisodeId()}&site=animefire preload="auto" controls="" style="width: 100%; height: 100%;"></video>
+      </div>`
+    }
+  }
+
   return (
-    <div>
+    <>
+      <div id="player">
       <ReactPlayer
         url={`${
           instance.defaults.baseURL
@@ -35,5 +56,10 @@ export function WatchView() {
         controls
       />
     </div>
+    <div id="playerSelect">
+      <button onClick={handleClick} className="active button">Player 1</button>
+      <button onClick={handleClick} className="button">Player 2</button>
+    </div>
+    </>
   );
 }
