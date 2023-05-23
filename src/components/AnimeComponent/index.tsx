23a77434cea315a896f5d749163b78abbc8e84e5
João Pedro <jaumgamer223@gmail.com>
2023-05-23 11:04:25 -0300

    tested new animation

--- src/components/AnimeComponent/index.tsx
+++ src/components/AnimeComponent/index.tsx
@@ -5,6 +5,20 @@ export function EpisodeView({ title, image, id = "" }) {
     const is = i.includes("Episódio");
     return is;
   };
+  const hiddenElements = document.querySelectorAll('.hidden')
+  const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting){
+        entry.target.classList.add('show')
+      }else{
+        entry.target.classList.remove('show')
+      }
+    })
+  })
+  hiddenElements.forEach(el => {
+    observer.observe(el)
+  })
   return (
     <Link to={isEpisode(title) ? `/watch${id}` : `${id}`} state={{episodes: id}}>
       <div className={isEpisode(title) ? "recentEpisode hidden" : "popular hidden"}>
